157e8468d8aa061d66b222b05fdc2499e64154e6
yongdoree <yongdoree@gmail.com>
2016-03-28 14:01:34 +0900

    Added home view

--- vanityfair/user/views/login.py
+++ vanityfair/user/views/login.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 from django.contrib.auth import authenticate, login
-
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 
 class LoginTemplateView(TemplateView):
 
@@ -9,14 +10,12 @@ class LoginTemplateView(TemplateView):
     def post(self, request):
         username = request.POST.get('username')
         password = request.POST.get('password')
-        
+
         user = authenticate(
             username=username,
             password=password,
         )
-        
+
         if user:
             login(request, user)
-        
         return redirect(reverse("home"))
-
