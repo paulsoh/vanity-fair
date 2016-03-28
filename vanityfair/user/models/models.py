8c6705ad00d741000a6d88d863a67d896bfdbc53
yongdoree <yongdoree@gmail.com>
2016-03-28 13:38:10 +0900

    modify: made migrations, added db.sqlite3 to .gitignore
    - models.py, settings.py - added installed_apps
    - Added Makefile: make manage can be used to makemigrations and migrate

--- vanityfair/user/models/models.py
+++ vanityfair/user/models/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    
+
     profile_image = models.ImageField(
         blank=True,
         null=True,
--- vanityfair/vanityfair/settings.py
+++ vanityfair/vanityfair/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'debug_toolbar',
 
+    'vanityfair',
     'user',
 
 ]
