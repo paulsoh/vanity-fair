86d7a1b1316df9de13f040b15c41979976333927
yongdoree <yongdoree@gmail.com>
2016-03-28 15:05:23 +0900

    modify: implemented Post model

--- vanityfair/post/models/post.py
+++ vanityfair/post/models/post.py
@@ -1,5 +1,28 @@
 from django.db import models
 
+from user.models import User
+
 
 class Post(models.Model):
-    pass
+
+    user = models.ForeignKey(
+        User,
+    )
+
+    title = models.CharField(
+        max_length=40,
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    class Meta:
+        verbose_name = "포스트"
+        verbose_name_plural = verbose_name
