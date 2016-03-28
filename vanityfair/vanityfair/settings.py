96a27e143af94c4b521fef01f87c37315e73f2ef
yongdoree <yongdoree@gmail.com>
2016-03-29 01:08:21 +0900

    modify: settings and urls to store and server images in posts

--- vanityfair/vanityfair/settings.py
+++ vanityfair/vanityfair/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'user.User'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'dist', 'media')
--- vanityfair/vanityfair/urls.py
+++ vanityfair/vanityfair/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from vanityfair.views import HomeTemplateView
 from user.views import *
@@ -17,4 +19,4 @@ urlpatterns = [
     url(r'^logout/', LogoutView.as_view(), name="logout"),
     url(r'^login/', LoginTemplateView.as_view(), name="login"),
     url(r'^signup/', SignupTemplateView.as_view(), name="signup"),
-]
+] + static(settings.MEDIA_URL, document_ROOT=settings.MEDIA_ROOT)
