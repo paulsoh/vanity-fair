8eb34c4ac2770da9560be01ffb1e5da745e8e78b
yongdoree <yongdoree@gmail.com>
2016-03-28 20:02:57 +0900

    init: Tag model(Many to many relationship with Post)

---
+++ vanityfair/post/models/tag.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Tag(models.Model):
+
+    name = models.CharField(
+        max_length=25,
+    )
+    
+    def __str__(self):
+        return "#{tag_name}".format(
+            tag_name=self.name
+        )
