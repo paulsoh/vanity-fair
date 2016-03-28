1d1084ec6585a297877d68e4a072f8ec26a7519e
yongdoree <yongdoree@gmail.com>
2016-03-28 13:27:36 +0900

    init: added development settings, django-extensions, toolbar

--- vanityfair/vanityfair/settings.py
+++ vanityfair/vanityfair/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+    'debug_toolbar',
+
+    'user',
+
 ]
 
 MIDDLEWARE_CLASSES = [
