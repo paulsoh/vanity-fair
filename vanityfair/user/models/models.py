3a90fff3d2baa4977fca27d56c4074596fffdf16
yongdoree <yongdoree@gmail.com>
2016-03-28 13:33:48 +0900

    init: added user model.py

---
+++ vanityfair/user/models/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    
+    profile_image = models.ImageField(
+        blank=True,
+        null=True,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    class Meta:
+        verbose_name = "User info"
+        verbose_name_plural = verbose_name
