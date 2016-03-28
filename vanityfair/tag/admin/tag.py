e853e4505ed9d9ebe571b03f27d47027e1d46734
yongdoree <yongdoree@gmail.com>
2016-03-28 21:42:18 +0900

    modify: moved tag admin to tag application

---
+++ vanityfair/tag/admin/tag.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from tag.models import Tag
+
+
+@admin.register(Tag)
+class TagModelAdmin(admin.ModelAdmin):
+
+    list_display = admin.ModelAdmin.list_display + (
+        'name',
+        'post',
+    )
