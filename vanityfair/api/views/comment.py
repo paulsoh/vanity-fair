9d6df457aba08a25e475d13a3004d40632910316
yongdoree <yongdoree@gmail.com>
2016-03-30 21:49:57 +0900

    modify: fixed comment feature, fixed js so that content is showed

--- vanityfair/api/views/comment.py
+++ vanityfair/api/views/comment.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,6 +10,8 @@ from api.serializers import CommentSerializer
 
 
 class CommentAPIView(APIView):
+
+    # permission_classes = (AllowAny,)
 
     def get(self, request, **kwargs):
         post = Post.objects.get(
--- vanityfair/vanityfair/static/vanityfair/js/detail.js
+++ vanityfair/vanityfair/static/vanityfair/js/detail.js
@@ -3,13 +3,14 @@
     $(document).ready(function(){
         var comment_element = $("#comment");
         var post_id = comment_element.data("post-id");
-
         $.ajax({
             url: "/api/posts/" + post_id + "/comments/",
             type: "GET",
-            success: function(comment){
-                for(var i=0; i<comment.length; i++){
-                    var comment = comment[i];
+            success: function(comments){
+                console.log(comments);
+                for(var i = 0; i < comments.length; i++) {
+                    var comment = comments[i];
+                    console.log(comment.content);
                     comment_element.append(
                         "<p>"+comment.content+
                         " by "+comment.user+
@@ -18,5 +19,63 @@
                 }
             }
         });
+
+		function getCookie(name) {
+			var cookieValue = null;
+			if (document.cookie && document.cookie != '') {
+				var cookies = document.cookie.split(';');
+				for (var i = 0; i < cookies.length; i++) {
+					var cookie = jQuery.trim(cookies[i]);
+					// Does this cookie string begin with the name we want?
+					if (cookie.substring(0, name.length + 1) == (name + '=')) {
+						cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+						break;
+					}
+				}
+			}
+			return cookieValue;
+		}
+
+        function csrfSafeMethod(method) {
+            // these HTTP methods do not require CSRF protection
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+        
+		var csrftoken = getCookie('csrftoken');
+
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            }
+        });
+
+
+        var $submit = $("#submit");
+        var $comment = $("textarea#comment");
+        
+        console.log(csrftoken)
+
+        $submit.click(function(){
+            console.log($comment.val());
+            $.ajax({
+                url: "/api/posts/" + post_id + "/comments/",
+                type: "POST",
+                data: {
+                    "content": $comment.val(),
+                    "post": post_id
+                }
+            })
+            .done(function(data, textStatus, jqXHR){
+                console.log("Http request suceeded: "+jqXHR.status);
+            })
+            .fail(function(jqXHR, textStatus, errorThrown){
+                console.log("Http request suceeded: "+jqXHR.status);
+                console.log(textStatus);
+                console.log(errorThrown);
+            })
+        });
     });
+
 })();
