bae53047c8c8bb37c9a5c8ae4625d45f76cd7cbc
yongdoree <yongdoree@gmail.com>
2016-03-31 15:41:52 +0900

    modify: pip packages to support Postgreql: postgresql_psycopg2         db settings in settings.py

--- vanityfair/vanityfair/settings.py
+++ vanityfair/vanityfair/settings.py
@@ -70,14 +70,26 @@ WSGI_APPLICATION = 'vanityfair.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'vfdb',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'admin',
+        'PASSWORD': 'th0611',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
