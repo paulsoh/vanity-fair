5492fad0d584d3c2746ee306b61b0486c247ad8f
yongdoree <yongdoree@gmail.com>
2016-03-31 16:16:23 +0900

    settings.py refactoring -> database setting with 'dj-database-url'

--- vanityfair/vanityfair/settings.py
+++ vanityfair/vanityfair/settings.py
@@ -70,6 +70,7 @@ WSGI_APPLICATION = 'vanityfair.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
 """
 DATABASES = {
     'default': {
@@ -78,17 +79,14 @@ DATABASES = {
     }
 }
 """
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'vfdb',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'admin',
-        'PASSWORD': 'th0611',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+    )
 }
 
 # Password validation
