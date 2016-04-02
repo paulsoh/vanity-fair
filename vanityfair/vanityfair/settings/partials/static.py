a72f65f55f613e3fefc2e7f73c4091bf4b002169
yongdoree <yongdoree@gmail.com>
2016-04-02 12:34:53 +0900

    Configuring pipeline settings;     deleted materialize css STYLESHEETS     modified pipeline settings

--- vanityfair/vanityfair/settings/partials/static.py
+++ vanityfair/vanityfair/settings/partials/static.py
@@ -5,5 +5,38 @@ from .application import *
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'dist', 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'dist', 'media')
+STATIC_ROOT = os.path.join(PROJECT_BASE_DIR, 'dist', 'static')
+
+MEDIA_ROOT = os.path.join(PROJECT_BASE_DIR, 'dist', 'media')
+
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'pipeline.finders.PipelineFinder',
+)
+
+
+PIPELINE = {
+    'STYLESHEETS': {
+        'hello': {
+            'source_filenames': (
+              'vanityfair/css/*.css',
+            ),
+            'output_filename': 'css/hello.css',
+            'extra_context': {
+                'media': 'screen,projection',
+            },
+        },
+    },
+    'JAVASCRIPT': {
+        'hello_js': {
+            'source_filenames': (
+              'vanityfair/js/*.js',
+            ),
+            'output_filename': 'js/hello.js',
+        }
+    }
+}
--- vanityfair/vanityfair/settings/production.py
+++ vanityfair/vanityfair/settings/production.py
@@ -3,4 +3,18 @@ from .partials import *
 DEBUG = False
 
 ALLOWED_HOSTS = [
+    '*',
 ]
+
+AWS_ACCESS_KEY_ID = 'AKIAJOFEWCW6KNOTJKDQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'doreee'
+
+STATICFILES_STORAGE = 'vanityfair.storage.S3PipelineCachedStorage'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_S3_CUSTOM_DOMAIN = 'd24o1a0vx62dlb.cloudfront.net'
+AWS_S3_URL_PROTOCOL = 'https'
+
+STATIC_URL = "https://d24o1a0vx62dlb.cloudfront.net/"
