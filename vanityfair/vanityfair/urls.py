a76f322095349949e09ce3eef564662fcb039b7d
yongdoree <yongdoree@gmail.com>
2016-04-05 22:44:20 +0900

    deleted static url due to s3boto and pipeline

--- vanityfair/vanityfair/urls.py
+++ vanityfair/vanityfair/urls.py
@@ -33,7 +33,4 @@ urlpatterns = [
 ] + static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
-    ) + static(
-            settings.STATIC_URL,
-            document_root=settings.STATIC_ROOT
-        )
+    )
